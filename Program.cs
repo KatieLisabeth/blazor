99f4f250abaf8d635a19d6497737408cffd08c98
KatieLisabeth <kat.lisabeth@gmail.com>
2025-04-08 15:10:24 +0200

    add local and session storage

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.SessionStorage;
+using Blazored.LocalStorage;
 using blazor;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddBlazoredLocalStorage();
 await builder.Build().RunAsync();
