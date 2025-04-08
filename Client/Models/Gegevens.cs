6e5f20157745449ef87023bec1ca1fca70f8ba00
KatieLisabeth <kat.lisabeth@gmail.com>
2025-04-08 11:56:27 +0200

    create gegevens

---
+++ Client/Models/Gegevens.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace blazor.Client.Models;
+
+public class Gegevens
+{
+    public int Getal { get; set; }
+    public string? Woord { get; set; }
+    public string? Derde { get; set; }
+}
